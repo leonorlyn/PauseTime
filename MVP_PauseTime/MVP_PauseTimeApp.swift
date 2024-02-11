412a397ce2e934e25ef74768f8765ae67a24565e
lena <107754913+leonorlyn@users.noreply.github.com>
2024-02-11 12:25:31 -0800

    Initial Commit

---
+++ MVP_PauseTime/MVP_PauseTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVP_PauseTimeApp.swift
+//  MVP_PauseTime
+//
+//  Created by Leonora on 2024/2/11.
+//
+
+import SwiftUI
+
+@main
+struct MVP_PauseTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
